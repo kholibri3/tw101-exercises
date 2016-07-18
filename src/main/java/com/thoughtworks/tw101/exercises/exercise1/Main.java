38947eb162b8f4cf8e03fbdda33c925d4c9800a6
Katie Ho <kho@umass.edu>
2016-07-18 12:26:29 -0700

    exercise #1 'print odd 1-100 & sums' completed

--- src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
@@ -2,8 +2,32 @@ package com.thoughtworks.tw101.exercises.exercise1;
 
 // Exercise #1: Print all odd numbers from 1 to 100 and their sum.
 
+class PrintOdd {
+
+    // keep track of sum while iterating over numbers, initialize to 0.
+    private int totalSum = 0;
+
+    // iterate over numbers 1 to 100, use mod to check if they are odd, add them to the sum if so
+    public void iterateSum() {
+        for(int i=1; i<=100; i++){
+            if(i%2!=0){
+                System.out.print(i + "    ");
+                totalSum+=i;
+                System.out.println("Current Sum: " + totalSum);
+            }
+        }
+    }
+}
+
 public class Main {
+
     public static void main(String[] args) {
 
+        System.out.println("\nPrinting odd numbers from 1 to 100 and their sum...\n");
+        PrintOdd odds = new PrintOdd();
+        odds.iterateSum();
+
+        System.out.println("DONE!\n");
     }
+
 }
