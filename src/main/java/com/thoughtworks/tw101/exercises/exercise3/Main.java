c8b9a5d67ba46b8902b01f4540c6ab53cfed6d9f
Katie Ho <kho@umass.edu>
2016-07-18 12:39:42 -0700

    exercise #3 'function <of> return odd sum btwn start and end values' completed

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/Main.java
@@ -7,9 +7,13 @@ public class Main {
 
     public static void main(String[] args) {
         OddSum oddSum = new OddSum();
+        oddSum.setInfo(1, 100);
 
-        Integer sum = oddSum.of(1, 100);
+        int start = oddSum.getStartVal();
+        int end = oddSum.getEndVal();
+        int sum = oddSum.of(start, end);
 
-        System.out.println(sum);
+        System.out.println("\nSum of all odd integers from " + start + " to " + end + ": " + sum);
+        System.out.println("DONE!\n");
     }
 }
