0f1a4e8270ecef44b3d120ac0fa00fb56e378f72
Katie Ho <kho@umass.edu>
2016-07-18 13:12:03 -0700

    exercise #5 'find avg area of 5 rectangles' completed

--- src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
@@ -3,6 +3,18 @@ package com.thoughtworks.tw101.exercises.exercise5;
 public class RectangleAverager {
 
     public float averageArea(Rectangle[] rectangles) {
-        return 0;
+
+        // keep track of area sum, initialize to 0
+        float totalArea = 0;
+
+        // iterate over array of rectangles and find total area
+        for(int i = 0; i<rectangles.length; i++) {
+           totalArea+=rectangles[i].area();
+        }
+
+        // divide by number of rectangles (ie. array's length) to calc average area
+        float avgArea = totalArea/rectangles.length;
+        return avgArea;
+
     }
 }
