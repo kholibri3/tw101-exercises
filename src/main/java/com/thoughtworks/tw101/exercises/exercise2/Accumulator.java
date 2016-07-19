55157e2e4c30d1b2e588ed2899cd00c2a3106e8b
Katie Ho <kho@umass.edu>
2016-07-18 22:14:56 -0700

    exercise #2 changed count variable to static after reading Head First Java

--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
@@ -3,7 +3,7 @@ package com.thoughtworks.tw101.exercises.exercise2;
 public class Accumulator {
 
     // tally up how many times increment method is called, initialize to 0
-    private int count = 0;
+    private static int count = 0;
 
     public void increment() {
         System.out.println("Increment method called!");
