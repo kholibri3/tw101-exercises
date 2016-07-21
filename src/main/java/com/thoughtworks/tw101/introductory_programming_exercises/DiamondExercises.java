955019cc4e9d9acaf651a38370681ef2503657ee
Katie Ho <kho@umass.edu>
2016-07-20 22:03:10 -0700

    completed isoceles triangles & diamonds (much code is repeated, due to java out of memory error/not enough heap space?)

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
@@ -14,7 +14,38 @@ public class DiamondExercises {
 //            *****
     private static void drawAnIsoscelesTriangle(int n) {
 
+        int numSpaces = n;
+        int numStars = 1;
+        int counter = 0;
+
+        for(int i=1; i<=n; i++) {
+            StringBuilder line = new StringBuilder();
+
+            // call helper method for spaces
+            printSpaces(0, numSpaces, line);
+
+            while(counter!=numStars) {
+                line.append("*");
+                counter++;
+            }
+
+            // call helper method for spaces
+            printSpaces(0, numSpaces, line);
+
+            System.out.println(line.toString());
+            counter = 0;
+            numSpaces-=1;
+            numStars+=2;
+        }
     }
+
+    // helper method for printing isoceles triangles and diamonds
+    private static void printSpaces(int i, int numSpaces, StringBuilder line) {
+        for(i=0; i<numSpaces; i++) {
+            line.append(" ");
+        }
+    }
+
 
 //    Diamond
 //    Given a number n, print a centered diamond. Example for n=3:
@@ -24,7 +55,58 @@ public class DiamondExercises {
 //             ***
 //              *
     private static void drawADiamond(int n) {
+        int numSpaces = n;
+        int numStars = 1;
+        int counter = 0;
+        int afterMiddleRow = 0;
 
+        for(int i=1; i<=n; i++) {
+            StringBuilder line = new StringBuilder();
+
+            // call helper method for spaces
+            printSpaces(0, numSpaces, line);
+
+            while(counter!=numStars) {
+                line.append("*");
+                counter++;
+            }
+
+            // call helper method for spaces
+            printSpaces(0, numSpaces, line);
+
+            System.out.println(line.toString());
+            counter = 0;
+            numSpaces-=1;
+
+            if(i==n-1)
+                afterMiddleRow = numStars;
+
+            numStars+=2;
+        }
+
+        numSpaces = 2;
+        numStars = afterMiddleRow;
+        counter = 0;
+
+        for(int i=n-1; i>0; i--) {
+            StringBuilder line = new StringBuilder();
+
+            // call helper method for spaces
+            printSpaces(0, numSpaces, line);
+
+            while(counter!=numStars) {
+                line.append("*");
+                counter++;
+            }
+
+            // call helper method for spaces
+            printSpaces(0, numSpaces, line);
+
+            System.out.println(line.toString());
+            counter = 0;
+            numSpaces+=1;
+            numStars-=2;
+        }
     }
 
 //    Diamond with Name
@@ -36,6 +118,64 @@ public class DiamondExercises {
 //            ***
 //             *
     private static void drawADiamondWithYourName(int n) {
+        int numSpaces = n;
+        int numStars = 1;
+        int counter = 0;
+        int afterMiddleRow = 0;
 
+        for(int i=1; i<n; i++) {
+            StringBuilder line = new StringBuilder();
+
+            // call helper method for spaces
+            printSpaces(0, numSpaces, line);
+
+            while(counter!=numStars) {
+                line.append("*");
+                counter++;
+            }
+
+            // call helper method for spaces
+            printSpaces(0, numSpaces, line);
+
+            System.out.println(line.toString());
+            counter = 0;
+            numSpaces-=1;
+
+            if(i==n-1)
+                afterMiddleRow = numStars;
+
+            numStars+=2;
+        }
+
+        numSpaces = 2;
+        int spacesAroundName = (numStars+numSpaces)/2-2;
+        numStars = afterMiddleRow;
+        counter = 0;
+
+        StringBuilder nameLine = new StringBuilder();
+        printSpaces(0, spacesAroundName, nameLine);
+        nameLine.append("Katie");
+        printSpaces(0, spacesAroundName, nameLine);
+        System.out.println(nameLine.toString());
+
+        for(int i=n-1; i>0; i--) {
+            StringBuilder line = new StringBuilder();
+
+            // call helper method for spaces
+            printSpaces(0, numSpaces, line);
+
+            while(counter!=numStars) {
+                line.append("*");
+                counter++;
+            }
+
+            // call helper method for spaces
+            printSpaces(0, numSpaces, line);
+
+            System.out.println(line.toString());
+            counter = 0;
+            numSpaces+=1;
+            numStars-=2;
+        }
     }
 }
--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
@@ -16,4 +16,7 @@ public class PrimeFactors {
     private static List<Integer> generate(int n) {
         return null;
     }
+
+
+    //Collections.sort
 }
